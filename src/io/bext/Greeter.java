3f0dbefd77a1e13bdd4b690be089c2e533fb4a63
jalbertomr <jalbertomr@yahoo.com>
2018-03-17 16:13:16 -0600

    + comportamiento myLambdaFunction pasada como parametro

--- src/io/bext/Greeter.java
+++ src/io/bext/Greeter.java
@@ -12,6 +12,20 @@ public class Greeter {
         HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
         greeter.greet(holaMundoGreeting);
         greeter.greet(helloWorldGreeting);
+
+        MyLambda myLambdaFunction       = () -> System.out.println("Hola desde lambda function");
+        InterGreeting myLambdafunction2 = () -> System.out.println("Hola desde lambda function");
+
+        greeter.greet(myLambdafunction2);
+
+        MyAdd myAddLambda = (int a, int b) -> a + b;
     }
 }
 
+interface MyLambda {
+    void perform();
+}
+
+interface MyAdd {
+    int add(int a, int b);
+}
