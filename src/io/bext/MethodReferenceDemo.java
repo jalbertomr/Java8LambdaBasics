a37975a6e0327e4c88a60ee15ca7cf3265373393
jalbertomr <jalbertomr@yahoo.com>
2018-03-18 13:25:03 -0600

    + MethodReference, Thread - Method Reference

--- src/io/bext/MethodReferenceDemo.java
+++ src/io/bext/MethodReferenceDemo.java
@@ -4,11 +4,11 @@ public class MethodReferenceDemo {
 
     public static void main(String... args) {
 
-        Thread t = new Thread(() -> printMessage());  // a sustituir por method reference
+        Thread t = new Thread(MethodReferenceDemo::printMessage);  // Method Reference
         t.start();
     }
 
     public static void printMessage() {
-        System.out.println("Hola desde Thread-lambda");
+        System.out.println("Hola desde Thread-Method Reference");
     }
 }
