42bc93a0e2a38e5f7318cfbe6db81a4321f3cdfa
jalbertomr <jalbertomr@yahoo.com>
2018-03-18 00:12:23 -0600

    + WrapperLambda captura Exception

--- src/io/bext/ExceptionHandlingDemo.java
+++ src/io/bext/ExceptionHandlingDemo.java
@@ -3,17 +3,12 @@ package io.bext;
 import java.util.function.BiConsumer;
 
 public class ExceptionHandlingDemo {
+
     public static void main(String... args) {
         int [] someNumbers = {1,2,3,4};
         int key = 0;
 
-        process(someNumbers, key, (v,k) -> {
-            try{
-                System.out.println(v / k);
-            }catch(ArithmeticException e){
-                System.out.println("Error: Exception Aritmetica");
-            }
-        });
+        process(someNumbers, key, wrapperLambda((v, k) -> System.out.println( v / k)));
     }
 
     private static void process(int[] someNumbers, int key, BiConsumer<Integer,Integer> consumer) {
@@ -21,4 +16,15 @@ public class ExceptionHandlingDemo {
             consumer.accept(i, key);
         }
     }
+
+    private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> consumer) {
+        return (v ,k ) -> {
+            try {
+                consumer.accept(v, k);
+            }catch(ArithmeticException a) {
+                System.out.println("Excepcion Aritmetica atrapada en WrapperLambda");
+            }
+        };
+    }
 }
+
