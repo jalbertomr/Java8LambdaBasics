100946186ac5b655cec1349082d123c08774bc19
jalbertomr <jalbertomr@yahoo.com>
2018-03-17 19:07:59 -0600

    + Inferencia de Types del Lambda...

--- src/io/bext/TypeInferenceDemo.java
+++ src/io/bext/TypeInferenceDemo.java
@@ -4,9 +4,15 @@ public class TypeInferenceDemo {
     public static void main(String... args) {
 
         //StringLengthLambda myLambda = (String s) -> s.length();    //La interface funcional al tener un solo metodo sabe que tipo de dato parametriza
-        StringLengthLambda myLambda = (s) -> s.length();
+        //StringLengthLambda myLambda = (s) -> s.length();
+        StringLengthLambda myLambda = s -> s.length();
         System.out.println(myLambda.getLength("Hola miLambda getlength()"));
 
+        printLambda(s -> s.length());
+    }
+
+    public static void printLambda(StringLengthLambda lambda) {
+        System.out.println( lambda.getLength("Dato para lambda"));
     }
 
     interface StringLengthLambda {
