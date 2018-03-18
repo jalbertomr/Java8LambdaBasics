0bcff19e3862a21c56a52195115b8033b845a20c
jalbertomr <jalbertomr@yahoo.com>
2018-03-18 10:50:51 -0600

    + "This" Demo, dentro de metodo de clase, Lambda llamada desde function(..,Interface)

--- src/io/bext/ThisReferenceDemo.java
+++ src/io/bext/ThisReferenceDemo.java
@@ -10,6 +10,15 @@ public class ThisReferenceDemo {
                 //System.out.println(this);     //ahora hace referencia al this del entorno, lo cual es error
             });
 
+        thisReferenceDemo.execute();
+    }
+
+    private void execute() {
+        this.doProcess(11, i -> {
+            System.out.println(".execute() valor de i:" + i);
+            System.out.println(this);            //hace referencia a io.bext.ThisReferenceDemo
+        });
+
     }
 
     private void doProcess(int i, Process p) {
